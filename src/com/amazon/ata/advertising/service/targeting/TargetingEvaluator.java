1538755d214d25c4bd2f64527a9c26f4600e5027
chmsly <chris.c.hemsley@gmail.com>
2024-02-25 09:27:16 -0700

    MT2

--- src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
+++ src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
@@ -6,6 +6,8 @@ import com.amazon.ata.advertising.service.targeting.predicate.TargetingPredicate
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 /**
  * Evaluates TargetingPredicates for a given RequestContext.
@@ -14,6 +16,7 @@ public class TargetingEvaluator {
     public static final boolean IMPLEMENTED_STREAMS = true;
     public static final boolean IMPLEMENTED_CONCURRENCY = true;
     private final RequestContext requestContext;
+    private final ExecutorService executorService;
 
     /**
      * Creates an evaluator for targeting predicates.
@@ -21,6 +24,7 @@ public class TargetingEvaluator {
      */
     public TargetingEvaluator(RequestContext requestContext) {
         this.requestContext = requestContext;
+        this.executorService = Executors.newFixedThreadPool(4);
     }
 
     /**
@@ -29,12 +33,36 @@ public class TargetingEvaluator {
      * @param targetingGroup Targeting group for an advertisement, including TargetingPredicates.
      * @return TRUE if all the TargetingPredicates evaluate to TRUE against the RequestContext, FALSE otherwise.
      */
-    public TargetingPredicateResult evaluate(TargetingGroup targetingGroup) {
-        boolean allTruePredicates =  targetingGroup.getTargetingPredicates()
-                .stream()
-                .allMatch(targetingPredicate -> targetingPredicate.evaluate(requestContext).isTrue());
+//    public TargetingPredicateResult evaluate(TargetingGroup targetingGroup) {
+//        boolean allTruePredicates =  targetingGroup.getTargetingPredicates()
+//                .stream()
+//                .allMatch(targetingPredicate -> targetingPredicate.evaluate(requestContext).isTrue());
+//
+//        return allTruePredicates ? TargetingPredicateResult.TRUE :
+//                TargetingPredicateResult.FALSE;
+//    }
 
-        return allTruePredicates ? TargetingPredicateResult.TRUE :
-                TargetingPredicateResult.FALSE;
+    public TargetingPredicateResult evaluate(TargetingGroup targetingGroup) {
+        List<Callable<Boolean>> tasks = targetingGroup.getTargetingPredicates().stream()
+                .map(predicate -> (Callable<Boolean>) () -> predicate.evaluate(requestContext).isTrue())
+                .collect(Collectors.toList());
+
+        try {
+            List<Future<Boolean>> futures = executorService.invokeAll(tasks);
+
+            for (Future<Boolean> future : futures) {
+                // If any of the predicates is false, return FALSE
+                if (!future.get()) {
+                    return TargetingPredicateResult.FALSE;
+                }
+            }
+            // All predicates returned true
+            return TargetingPredicateResult.TRUE;
+        } catch (InterruptedException | ExecutionException e) {
+            // Handle the exception appropriately - this could be a log, rethrow, or another action
+            e.printStackTrace();
+            return TargetingPredicateResult.FALSE;
+        }
     }
+
 }
